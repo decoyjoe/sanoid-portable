f46a2c8e26788e75c82f24eca7b8e59d75de0d7e
Joseph Larionov <decoyjoe@gmail.com>
2024-11-29 19:31:04 -0800

    Add a proper help option to print usage information

--- sanoid-portable.pl
+++ sanoid-portable.pl
@@ -14,23 +14,9 @@ my $sanoid_version = $versions->{'Sanoid'};
 my $packaging_revision = $versions->{'PackagingRevision'};
 my $apperl_version = $versions->{'APPerl'};
 my $sanoid_portable_version = "$sanoid_version-$packaging_revision";
+my $perl_version = $^V; # Built-in variable for Perl version
 
-# Only output the version number when using -V|--version
-my $print_version_only = 0;
-GetOptions('V|version' => \$print_version_only);
-
-if ($print_version_only) {
-    print "$sanoid_portable_version\n";
-    exit 0;
-}
-
-# Print versions
-print "sanoid-portable: $sanoid_portable_version\n";
-print "Sanoid: $sanoid_version\n";
-print "Perl: $^V\n";  # Built-in variable for Perl version
-print "APPerl: $apperl_version\n";
-
-my $usage = <<'USAGE';
+my $usage = <<'EOF';
 This binary executes sanoid, syncoid, or findoid based on the name of the symbolic link invoked.
 
 Create symbolic links to use the different tools:
@@ -46,6 +32,34 @@ Then invoke the symlink:
     ./syncoid --help
     ./findoid --help
 
-USAGE
+Options:
+    -V, --version   Print version information and exit
+    -h, --help      Print this help message and exit
 
-print "\n$usage";
+EOF
+
+my $all_versions = <<"EOF";
+sanoid-portable: $sanoid_portable_version
+sanoid: $sanoid_version
+Perl: $perl_version
+APPerl: $apperl_version
+EOF
+
+# Parse command-line options
+my $print_version_only = 0;
+my $print_help = 0;
+GetOptions(
+    'V|version' => \$print_version_only,
+    'h|help' => \$print_help
+);
+
+if ($print_version_only) {
+    print "$sanoid_portable_version\n";
+    exit 0;
+}
+
+if ($print_help) {
+    print "$all_versions\n";
+    print $usage;
+    exit 0;
+}
